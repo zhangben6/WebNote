cf8d16f4d3b80d30f10310de1f6a8009e94b04d5
zhangben6 <1457287508@qq.com>
2018-12-13 08:50:41 +0800

    day8作业

---
+++ DAY8/9x9乘法表.py
@@ -0,0 +1,38 @@
+
+
+# for x in range(1,10):
+#     for y in range(1,x+1):
+#         print('%dx%d=%2d'%(y,x,x*y),end=' ')
+#     print()
+
+
+
+
+                          
+                    #      *
+                    #     ***4
+
+                    #    *****
+                    #   *******
+# n = int(input('>>>'))
+# for x in range(1,n+1):
+#     for y in range(n-x+1):
+#         print(' ',end='')
+#     for z in range(1,2*x):
+#         print('*',end='')
+#     print()
+
+# n = int(input('>>>'))
+# prime_flag=0
+# for x in range(2,n):
+#     if n % 2 == 0:
+#         prime_flag=1
+#         break
+# if prime_flag == 1:
+#     print('不是素数')
+# else:
+#     print('是素数')
+
+
+
+
